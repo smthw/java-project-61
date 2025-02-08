e020ab6977e0e5969bf42d20216316751c0de1f0
Dmitry Andrusishin <andrusishindv@gmail.com>
2025-02-08 13:15:27 +0300

    fix magic number in Utils

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -5,11 +5,13 @@ import java.util.Random;
 public class Utils {
     static final Random RANDOM = new Random();
 
+    static final int DEFAULT_VAL_FOR_RANDOM = 10;
+
     public static int randomNumber(int num) {
         return RANDOM.nextInt(1, num);
     }
 
     public static int randomNumber() {
-        return randomNumber(10);
+        return randomNumber(DEFAULT_VAL_FOR_RANDOM);
     }
 }
