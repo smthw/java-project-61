03a4940ca1fd2ecb852f8e3f2b951f444318b9a4
smthw <andrusishindv@gmail.com>
2026-08-16 20:55:35 +0300

    feat(core): implement user greeting logic
    • Create Cli class to handle user interaction
    • Update App entry point to capture username
    • Configure build.gradle.kts to enable standard input for the run task
    • Add Makefile for build automation

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String greeting() {
+        System.out.println("Welcome to the Brain Games!");
+
+        System.out.print("May I have your name? ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();
+
+        System.out.println("Hello, " + userName + "!");
+
+        return userName;
+    }
+}
